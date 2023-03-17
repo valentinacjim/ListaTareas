d80a639259b05997c6ac333ad2b4f26fc775af10
valentinacataldo <100451232@alumnos.uc3m.es>
2023-03-17 12:22:03 +0100

    add tasks

--- www/script.js
+++ www/script.js
@@ -17,29 +17,67 @@ async function loadTasks() {
   const data = await response.json();
   data.forEach((task) => {
     taskList.push(task);
+    console.log(taskList);
   });
 }
 
 const add = () => {
   task_name = document.querySelector("#task-name").value;
-  if (task_name = "") {
+  console.log(task_name);
+
+  tasks_name = []
+  
+  // console.log(tasksjson);
+  if (task_name === '') {
     return;
   }
-  taskList.push({
-    id: taskList.length + 1,
-    name: task_name,
-    done: false,
-  });
+  else{
+    for (let i = 0; i < taskList.length; i++) {
+      if(taskList[i].title === task_name){
+        return;
+      }
+    }
+    taskList.push({
+      id: taskList.length + 1,
+      title: task_name,
+      done: false,
+    });
+  add_task();
+  }
+  
+  
+  console.log(taskList);
 }
 
 const remove = () => {}
 
 const toggleDone = () => {}
 
+const add_task = () => {
+  tasks= document.querySelector("#tasks-container");
+  tasks.innerHTML = '';
+  taskList.forEach((task) => {
+    // console.log();
+    tasks.innerHTML += `
+      <div class="task-container">
+     <h3>${task.title}</h1>
+     </div>
+    `;	
+    // console.log(task.title);   
+  });
+
+}
+
+async function load(){
+  await loadTasks();
+  add_task();
+  // console.log(tasks);
+}
+
 const addButton = document.querySelector("#fab-add");
 
-addButton.addEventListener("touchend", add);
+addButton.addEventListener("click", add);
 
-loadTasks();
-console.log(taskList);
+load();
+// console.log(taskList);
 
